875d0d967d533452c45bb24d64ba8e9062532e5b
Courtney Adams <cadams8238@gmail.com>
2018-08-02 12:37:37 -0700

    added mergesort function

---
+++ mergesort.js
@@ -0,0 +1,57 @@
+// const str = '89 30 25 32 72 70 51 42 25 24 53 55 78 50 13 40 48 32 26 2 14 33 45 72 56 44 21 88 27 68 15 62 93 98 73 28 16 46 87 28 65 38 67 16 85 63 23 69 64 91 9 70 81 27 97 82 6 88 3 7 46 13 11 64 76 31 26 38 28 13 17 69 90 1 6 7 64 43 9 73 80 98 46 27 22 87 49 83 6 39 42 51 54 84 34 53 78 40 14 5';
+const str = '50 40 10 30 25 5 50 40 10 30 25 5';
+
+const array = str.split(' ').map(el => Number(el));
+
+
+let count = 0;
+
+function mSort(array) {
+  if (array.length <= 1) {
+    return array;
+  }
+
+  const middle = Math.floor(array.length/2);
+  let left = array.slice(0, middle),
+      right = array.slice(middle, array.length);
+
+  left = mSort(left);
+  right = mSort(right);
+  count++;
+  console.log(count);
+  return merge(left, right, array);
+};
+
+
+function merge(left, right, array) {
+  let leftIndex = 0,
+      rightIndex = 0,
+      outputIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      array[outputIndex] = left[leftIndex];
+      outputIndex++;
+      leftIndex++;
+    }
+    else {
+      array[outputIndex] = right[rightIndex];
+      outputIndex++;
+      rightIndex++;
+    }
+  }
+
+  for (let i = leftIndex; i < left.length; i++) {
+    array[outputIndex] = left[i];
+    outputIndex++;
+  }
+
+  for (let i = rightIndex; i < right.length; i++) {
+    array[outputIndex] = right[i];
+    outputIndex++;
+  }
+
+  return array;
+}
+
+console.log(mSort(array));
